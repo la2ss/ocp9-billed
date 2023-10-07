3d74d693efba285f44b2099e85710ce4c3560da2
Lassina Sylla <lass390225@gmail.com>
2023-10-07 15:13:04 +0200

    ajout verifivation nouvelle note

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,13 +2,20 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {fireEvent, getByTestId, logDOM, screen, waitFor,
+} from "@testing-library/dom"
+import userEvent from "@testing-library/user-event"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { bill } from "../fixtures/bill.js"
+import { ROUTES_PATH, ROUTES } from "../constants/routes.js"
+import Bills from "../containers/Bills"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import mockStore from "../__mocks__/store"
 
-import router from "../app/Router.js";
+import router from "../app/Router.js"
+
+jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -30,10 +37,22 @@ describe("Given I am connected as an employee", () => {
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map((a) => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+    test("The button to create new bills should be present and redirect the user to another page", () => {
+      document.body.innerHTML = BillsUI({ data: bills })
+
+      const buttonNewBill = screen.getByTestId("btn-new-bill")
+
+      expect(buttonNewBill.textContent).toBe("Nouvelle note de frais")
+
+      fireEvent.click(buttonNewBill)
+      //we redirect the user
+      expect(document.body.innerHTML).not.toBe(BillsUI({ data: bills }))
+    })
+
   })
 })
